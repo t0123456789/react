1a6a916d517c4382a7d8abf2ec4f7b39ef2a2077
t0123456789 <15105077+t0123456789@users.noreply.github.com>
2019-12-10 23:09:32 +0000

    test

---
+++ precache-manifest.28554127b6788659b44e80283eefeb88.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "357e74091e0717143ca58e11ad145f19",
+    "url": "/react/index.html"
+  },
+  {
+    "revision": "2656b9d1340ae9fcbad0",
+    "url": "/react/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "02599b50f13167857094",
+    "url": "/react/static/js/2.e7d864ab.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/react/static/js/2.e7d864ab.chunk.js.LICENSE"
+  },
+  {
+    "revision": "2656b9d1340ae9fcbad0",
+    "url": "/react/static/js/main.3effcce9.chunk.js"
+  },
+  {
+    "revision": "2181bc5c10186044dec5",
+    "url": "/react/static/js/runtime-main.005b3384.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react/static/media/logo.5d5d9eef.svg"
+  }
+]);
